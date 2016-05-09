766af8fc6354c1af6b462336f388f1e97d333956
Martin Nally <mnally@apigee.com>
2016-05-09 12:26:13 -0700

    generate for teams (muster)

--- gen.sh
+++ gen.sh
@@ -7,6 +7,7 @@ gen_openapispec.py public/deployment.yaml > public/openapi/deployment.yaml
 gen_openapispec.py public/ssl.yaml > public/openapi/ssl.yaml 
 gen_openapispec.py public/shipyard-build.yaml > public/openapi/shipyard-build.yaml 
 gen_openapispec.py public/permissions.yaml > public/openapi/permissions.yaml 
+gen_openapispec.py public/teams.yaml > public/openapi/teams.yaml 
 gen_openapispec.py public/users.yaml > public/openapi/users.yaml 
 gen_openapispec.py public/mint.yaml > public/openapi/mint.yaml 
 
@@ -17,5 +18,6 @@ gen_html.py public/deployment.yaml > public/html/deployment.html
 gen_html.py public/ssl.yaml > public/html/ssl.html 
 gen_html.py public/shipyard-build.yaml > public/html/shipyard-build.html 
 gen_html.py public/permissions.yaml > public/html/permissions.html 
+gen_html.py public/teams.yaml > public/html/teams.html 
 gen_html.py public/users.yaml > public/html/users.html 
 gen_html.py public/mint.yaml > public/html/mint.html 
